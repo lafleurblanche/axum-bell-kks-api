4dde09bc86ac9773ea5a7db1d6cefad9562150f3
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-02-27 02:09:29 +0900

    20250227 chore:

--- axum-bell-kks-api/src/handler/bellkks.rs
+++ axum-bell-kks-api/src/handler/bellkks.rs
@@ -1,3 +1,9 @@
 pub mod bellkks01handler;
 pub mod bellkks02handler;
 pub mod bellkks03handler;
+pub mod bellkks04handler;
+pub mod bellkks05handler;
+pub mod bellkks06handler;
+pub mod bellkks07handler;
+pub mod bellkks08handler;
+pub mod bellkks09handler;
