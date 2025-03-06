c1648135faa49cd1d8d17fcd0277158beada5c45
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-07 00:23:14 +0900

    20250307 chore:

--- axum-bell-kks-api/src/domain/bellkks/model.rs
+++ axum-bell-kks-api/src/domain/bellkks/model.rs
@@ -192,3 +192,51 @@ pub struct BellKks12 {
     #[serde(rename = "staName")]
     pub sta_name: String,
 }
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct BellKks13 {
+    pub id: i32,
+    #[serde(rename = "routeID")]
+    pub route_id: String,
+    #[serde(rename = "staCode")]
+    pub sta_code: String,
+    #[serde(rename = "fromStaCode")]
+    pub from_sta_code: String,
+    #[serde(rename = "toStaCode")]
+    pub to_sta_code: String,
+    #[serde(rename = "staName")]
+    pub sta_name: String,
+}
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct BellKks14 {
+    pub id: i32,
+    #[serde(rename = "routeID")]
+    pub route_id: String,
+    #[serde(rename = "staCode")]
+    pub sta_code: String,
+    #[serde(rename = "fromStaCode")]
+    pub from_sta_code: String,
+    #[serde(rename = "toStaCode")]
+    pub to_sta_code: String,
+    #[serde(rename = "staName")]
+    pub sta_name: String,
+}
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct BellKks15 {
+    pub id: i32,
+    #[serde(rename = "routeID")]
+    pub route_id: String,
+    #[serde(rename = "staCode")]
+    pub sta_code: String,
+    #[serde(rename = "fromStaCode")]
+    pub from_sta_code: String,
+    #[serde(rename = "toStaCode")]
+    pub to_sta_code: String,
+    #[serde(rename = "staName")]
+    pub sta_name: String,
+}
