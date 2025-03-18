ee49ba1cec9e52fb3e278c4ad12c14ea607d25c7
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-03-18 21:08:32 +0900

    20250318 chore

---
+++ axum-bell-kks-api/src/handler/bellkks/bellkks19handler.rs
@@ -0,0 +1,97 @@
+use std::sync::Arc;
+
+use axum::{
+    extract::{Path, Query, State},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+
+use crate::domain::bellkks::model::BellKks19;
+
+use crate::{
+    schema::{FilterOptions},
+    AppState,
+};
+
+pub async fn bellkks19_list_handler(
+    opts: Option<Query<FilterOptions>>,
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let Query(opts) = opts.unwrap_or_default();
+
+    let limit = opts.limit.unwrap_or(30);
+    let offset = (opts.page.unwrap_or(1) - 1) * limit;
+
+    let query_result = sqlx::query_as!(
+        BellKks19,
+        r#"SELECT * FROM bell_kks19 ORDER by id LIMIT $1 OFFSET $2"#,
+        limit as i32,
+        offset as i32
+    )
+    .fetch_all(&data.db)
+    .await;
+
+    if query_result.is_err() {
+        let error_response = serde_json::json!({
+            "status": "fail",
+            "message": "Something bad happened while fetching all bellkks19 items",
+        });
+        return Err((StatusCode::INTERNAL_SERVER_ERROR, Json(error_response)));
+    }
+
+    let bellkks19list = query_result.unwrap();
+
+    let json_response = serde_json::json!({
+        "bellkks19list": bellkks19list
+    });
+    Ok(Json(json_response))
+}
+
+pub async fn get_bellkks19_handler(
+    Path(id): Path<i32>,
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query_result = sqlx::query_as!(BellKks19, r#"SELECT * FROM bell_kks19 WHERE id = $1"#, id)
+        .fetch_one(&data.db)
+        .await;
+
+    match query_result {
+        Ok(bellkks19) => {
+            let bellkks19_response = serde_json::json!({"status": "success","data": serde_json::json!({
+                "bellkks19": bellkks19
+            })});
+
+            return Ok(Json(bellkks19_response));
+        }
+        Err(_) => {
+            let error_response = serde_json::json!({
+                "status": "fail",
+                "message": format!("bellkks19 with ID: {} not found", id)
+            });
+            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        }
+    }
+}
+
+pub async fn get_stacode_bellkks19_handler(
+    Path(sta_code): Path<String>,
+    State(data): State<Arc<AppState>>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query_result = sqlx::query_as!(BellKks19, r#"SELECT * FROM bell_kks19 WHERE sta_code = $1"#, sta_code)
+        .fetch_one(&data.db)
+        .await;
+
+    match query_result {
+        Ok(bellkks19_stacode) => {
+            return Ok(Json(bellkks19_stacode));
+        }
+        Err(_) => {
+            let error_response = serde_json::json!({
+                "status": "fail",
+                "message": format!("bellkks19 with ID: {} not found", sta_code)
+            });
+            return Err((StatusCode::NOT_FOUND, Json(error_response)));
+        }
+    }
+}
