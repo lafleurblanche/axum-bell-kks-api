c9cb75ea07c2f1a7d8754a0083c044c1aba7d787
lafleurblanche <aquamint@squall.dti.ne.jp>
2025-04-01 23:54:51 +0900

    2050401 chore

--- axum-bell-kks-api/src/domain/bellkks/model.rs
+++ axum-bell-kks-api/src/domain/bellkks/model.rs
@@ -336,3 +336,35 @@ pub struct BellKks21 {
     #[serde(rename = "staName")]
     pub sta_name: String,
 }
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct BellKks22 {
+    pub id: i32,
+    #[serde(rename = "routeID")]
+    pub route_id: String,
+    #[serde(rename = "staCode")]
+    pub sta_code: String,
+    #[serde(rename = "fromStaCode")]
+    pub from_sta_code: String,
+    #[serde(rename = "toStaCode")]
+    pub to_sta_code: String,
+    #[serde(rename = "staName")]
+    pub sta_name: String,
+}
+
+#[derive(Debug, FromRow, Deserialize, Serialize)]
+#[allow(non_snake_case)]
+pub struct BellKks23 {
+    pub id: i32,
+    #[serde(rename = "routeID")]
+    pub route_id: String,
+    #[serde(rename = "staCode")]
+    pub sta_code: String,
+    #[serde(rename = "fromStaCode")]
+    pub from_sta_code: String,
+    #[serde(rename = "toStaCode")]
+    pub to_sta_code: String,
+    #[serde(rename = "staName")]
+    pub sta_name: String,
+}
